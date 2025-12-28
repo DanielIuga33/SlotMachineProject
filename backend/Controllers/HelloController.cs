3baa20044c2c6d2e62216abe02d36b52d184c9e7
Daniel Iuga <danieliuga33@yahoo.com>
2025-12-28 17:16:47 +0200

    The begining of the journey

---
+++ backend/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { mesaj = "Backend-ul este online!", data = DateTime.Now });
+        }
+    }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Servicii de bază
+builder.Services.AddControllers();
+
+// Adaugă asta pentru React (CORS)
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowReact", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors("AllowReact"); // Permite React-ului să vorbească cu C#
+app.MapControllers();
+
+app.Run();
