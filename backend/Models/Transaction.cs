aa884ac2dfa9b0a65d2ee9f93983f95d0de15b6b
Daniel Iuga <danieliuga33@yahoo.com>
2025-12-30 22:40:13 +0200

    working on transactions

---
+++ backend/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
